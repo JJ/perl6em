c5c158f840347d227441f5ae03ef05c790314955
JJ Merelo <jjmerelo@gmail.com>
2017-11-24 08:39:37 +0100

    Adds new repos to preso

--- utils/new_repos_perl.R
+++ utils/new_repos_perl.R
@@ -1,5 +1,7 @@
 library(ggplot2)
 library(ggthemes)
 
-data <- read.csv("new_repos.csv")
-ggplot()+geom_line(data=data,aes(x=as.numeric(row.names(data)),y=Perl,color='Perl',group=1))+geom_line(data=data,aes(x=as.numeric(row.names(data)),y=Perl6,color='Perl6',group=1))+theme_tufte()+xlab("Month")+ylab("New repos in GitHub")
+data <- read.csv("new_repos.csv", stringsAsFactors = FALSE)
+data$Date <- as.Date(paste0(data$Date,"/01"),format="%Y/%m/%d")
+ggplot()+geom_line(data=data,aes(x=data$Date,y=Perl,color='Perl',group=1))+geom_line(data=data,aes(x=data$Date,y=Perl6,color='Perl6',group=1))+theme_tufte()+xlab("Month")+ylab("New repos in GitHub")
+
