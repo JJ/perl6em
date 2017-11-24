8922c8209b59ae6757af17a344b59bb4f5898a8f
JJ Merelo <jjmerelo@gmail.com>
2017-11-24 10:24:53 +0100

    Reverting from an invented thing
    There is a "payload" field in the GitHub tables which contains this
    kind of information. I though there was one like that for every event,
    only every one has its own kind of information, which is not
    structured and thus goes to the "payload" field which we have to
    extract.

--- utils/bq.sh
+++ utils/bq.sh
@@ -1,7 +1,7 @@
 # Taken from https://stackoverflow.com/questions/41475499/how-to-measure-language-popularity-via-github-archive-data
-bq query --format json "SELECT COUNT(*) c, JSON_EXTRACT_SCALAR(payload, '$.create.base.repo.language') lang
+bq query --format json "SELECT COUNT(*) c, JSON_EXTRACT_SCALAR(payload, '$.pull_request.base.repo.language') lang
 FROM [githubarchive:month.201710]
-WHERE JSON_EXTRACT_SCALAR(payload, '$.create.base.repo.language') IS NOT NULL
+WHERE JSON_EXTRACT_SCALAR(payload, '$.pull_request.base.repo.language') IS NOT NULL
 GROUP BY 2
 ORDER BY 1 DESC
 LIMIT 100" 
