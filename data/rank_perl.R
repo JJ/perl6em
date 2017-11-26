dc9e7c4baa2d9628bce1a0e716503b178de96621
JJ Merelo <jjmerelo@gmail.com>
2017-11-26 19:17:04 +0100

    Generating new images with correct labels

--- data/rank_perl.R
+++ data/rank_perl.R
@@ -3,5 +3,5 @@ library(ggthemes)
 
 data <- read.csv("rank.csv", stringsAsFactors = FALSE)
 data$Date <- as.Date(paste0(data$Date,"/01"),format="%Y/%m/%d")
-ggplot()+geom_line(data=data,aes(x=data$Date,y=Perl,color='Perl',group=1))+geom_line(data=data,aes(x=data$Date,y=Perl6,color='Perl6',group=1))+theme_tufte()+xlab("Month")+ylab("New repos in GitHub")
+ggplot()+geom_line(data=data,aes(x=data$Date,y=Perl,color='Perl',group=1))+geom_line(data=data,aes(x=data$Date,y=Perl6,color='Perl6',group=1))+theme_tufte()+xlab("Month")+ylab("Pull requests in GitHub")
 
